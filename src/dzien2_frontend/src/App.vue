47b7432dbc9cc7dd02afd1c9067f154bd2f44bc9
WiktorBkb <wiktorbkbk@gmail.com>
2024-06-26 21:10:49 +0200

    dodanie wszystkich plików przez error

---
+++ src/dzien2_frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Blog from './components/Blog.vue';
+</script>
+
+<template>
+  <main class="container mx-auto">
+    <img src="/logo2.svg" alt="DFINITY logo" />
+    <br />
+    <br />
+    <Blog />
+  </main>
+</template>
